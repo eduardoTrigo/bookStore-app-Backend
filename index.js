3ab313da019a296badd023ea322f5ce7a8d35909
Eduardo Trigo <eduatrigo@gmail.com>
2025-06-02 22:43:24 -0300

    cracion y enrutamiento de endpoints de books

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const { default: mongoose } = require('mongoose')
 const { EXPRESS_CONFIG, MONGO_CONFIG } = require('./config')
 const authorRouter = require('./routes/author.routes')
+const bookRouter = require('./routes/book.routes')
 
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 mongoose.connect(MONGO_CONFIG.URI)
     .then(()=>console.log({message:'base de datos conectada'}))
