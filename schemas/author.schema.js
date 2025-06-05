b734fb0b19bf3c22ee90077150aac5f3ad1d95a5
Eduardo Trigo <eduatrigo@gmail.com>
2025-06-05 18:23:09 -0300

    creacion e implementacion de middlewares de validacion de datos de los modelos de Author, y refactor en las respectivas rutas de Author

--- schemas/author.schema.js
+++ schemas/author.schema.js
@@ -2,11 +2,22 @@ const z = require('zod')
 
 const validateDataAuthor = z.object({
     firstName: z.string({
-        required_error: "username is required",
-    }),
+        required_error: 'username is required',
+        invalid_type_error: 'First name must be a String',
+    }).min(1, 'First name cannot be empty'),
     lastName: z.string({
-        required_error: "lastname is required"
-    })
+        required_error: 'lastname is required',
+        invalid_type_error: 'Last name must be a string'
+    }).min(1, 'Last name cannot be empty'),
 })
 
-module.exports = validateDataAuthor
+const updateDataAuthor = z
+    .object({
+        firstName: z.string().min(1, 'First name cannot empty').optional(),
+        lastName: z.string().min(1, ' Last name cannot empty').optional()
+    })
+    .refine((data) => Object.keys(data).length > 0, {
+        message:'At least one field must be provided for update',
+    })
+
+module.exports = { validateDataAuthor , updateDataAuthor }
